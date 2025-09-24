c67d5dfd0d1a44bed675d335f6e6119d66a8791b
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-09-24 06:16:27 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,1 +1,6 @@
 --Week 6: Joins and Relationships
+-- Question 1: Get employee details with office code using INNER JOIN
+SELECT e.firstName, e.lastName, e.email, e.officeCode
+FROM employees e
+INNER JOIN offices o ON e.officeCode = o.officeCode;
+
